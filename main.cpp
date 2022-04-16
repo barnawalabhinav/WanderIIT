414d93905cac614822d55fd2e8225715afbba40e
vamshivangala <78156493+vamshivangala@users.noreply.github.com>
2022-04-16 21:50:56 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,63 @@
-#include "WanderIIT.hpp"
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
-WanderIIT *game = nullptr;
-
-int main ( int args, const char* argv[])
+int main(int argc, char *argv[])
 {
-    game = new WanderIIT();
-
-    game->init("WanderIIT", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1231, 687, 44, 140);
-    game->loadmedia();
-
-    while (game->running()) {
-        game->handleEvents();
-        game->update();
-        game->collison();
-        game->render();
+    SDL_Window *window = nullptr;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized: " << SDL_GetError();
     }
+    else
+    {
+        std::cout << "SDL video system is ready to go\n";
+    }
+    window = SDL_CreateWindow("C++ SDL2 Window", 20, 20, 640, 480, SDL_WINDOW_SHOWN);
 
-    game->clean();
-
+    SDL_Renderer *renderer = nullptr;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (TTF_Init() == -1)
+    {
+        std::cout << "Could not initailize SDL2_ttf, error: " << TTF_GetError() << std::endl;
+    }
+    else
+    {
+        std::cout << "SDL2_ttf system ready to go!" << std::endl;
+    }
+    TTF_Font *ourFont = TTF_OpenFont("./fonts/8bitOperatorPlus8-Regular.ttf", 32);
+    if (ourFont == nullptr)
+    {
+        std::cout << "Could not load font" << std::endl;
+        exit(1);
+    }
+    SDL_Surface *surfaceText = TTF_RenderText_Blended_Wrapped(ourFont, "Instructions \n nomber 1", {255, 255, 255}, 300);
+    SDL_Texture *textureText = SDL_CreateTextureFromSurface(renderer, surfaceText);
+    SDL_FreeSurface(surfaceText);
+    SDL_Rect rectangle;
+    rectangle.x = 20;
+    rectangle.y = 20;
+    rectangle.w = 800;
+    rectangle.h = 200;
+    bool gameIsRunning = true;
+    while (gameIsRunning)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                gameIsRunning = false;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, textureText, NULL, &rectangle);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyTexture(textureText);
+    SDL_DestroyWindow(window);
+    TTF_CloseFont(ourFont);
+    SDL_Quit();
     return 0;
 }
-
