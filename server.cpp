a28f08c9b7e87b0cc3a4f9d00070a33e1b82a188
vamshivangala <78156493+vamshivangala@users.noreply.github.com>
2022-04-19 00:55:02 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -2,10 +2,9 @@
 // programming
 #include "server.h"
 
-int server::chartoint(char ch)
-{
-	int x = ch - '0';
-	return x;
+int chartoint(char ch){
+    int x = ch - '0';
+    return x;
 }
 
 int server::setupConnection()
@@ -15,17 +14,16 @@ int server::setupConnection()
 	int addrlen = sizeof(address);
 
 	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0))
+		== 0) {
 		perror("socket failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Forcefully attaching socket to the port 8080
 	if (setsockopt(server_fd, SOL_SOCKET,
-				   SO_REUSEADDR | SO_REUSEPORT, &opt,
-				   sizeof(opt)))
-	{
+				SO_REUSEADDR | SO_REUSEPORT, &opt,
+				sizeof(opt))) {
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
@@ -34,20 +32,20 @@ int server::setupConnection()
 	address.sin_port = htons(PORT);
 
 	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-			 sizeof(address)) < 0)
-	{
+	if (bind(server_fd, (struct sockaddr*)&address,
+			sizeof(address))
+		< 0) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0)
-	{
+	if (listen(server_fd, 3) < 0) {
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-							 (socklen_t *)&addrlen)) < 0)
-	{
+	if ((new_socket
+		= accept(server_fd, (struct sockaddr*)&address,
+				(socklen_t*)&addrlen))
+		< 0) {
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
@@ -57,8 +55,8 @@ int server::setupConnection()
 
 void server::extract_data(char data[], int &x, int &y, int &w, int &h, int &won, int &quit, int &miles)
 {
-	x = 1000 * chartoint(data[0]) + 100 * chartoint(data[1]) + 10 * chartoint(data[2]) + chartoint(data[3]);
-	y = 1000 * chartoint(data[5]) + 100 * chartoint(data[6]) + 10 * chartoint(data[7]) + chartoint(data[8]);
+	x = 1000*chartoint(data[0])+ 100*chartoint(data[1])+10*chartoint(data[2])+chartoint(data[3]);
+	y = 1000*chartoint(data[5])+ 100*chartoint(data[6])+10*chartoint(data[7])+chartoint(data[8]);
 	w = chartoint(data[10]);
 	h = chartoint(data[12]);
 	won = chartoint(data[14]);
@@ -66,51 +64,42 @@ void server::extract_data(char data[], int &x, int &y, int &w, int &h, int &won,
 	miles = chartoint(data[18]);
 }
 
-char *server::sendData(Player *p)
+char* server::sendData(Player *p)
 {
-	string string_of_x;
+	string string_of_x; 
 	string string_of_y;
-	if (p->position.x < 10)
-	{
+	if(p->position.x<10){
 		string_of_x = ("0" + ("0" + ("0" + to_string(p->position.x))));
 	}
-	else if (p->position.x < 100)
-	{
-		string_of_x = ("0" + ("0" + to_string(p->position.x)));
+	else if(p->position.x<100){
+		 string_of_x = ("0" + ("0" + to_string(p->position.x)));
 	}
-	else if (p->position.x < 1000)
-	{
-		string_of_x = ("0" + to_string(p->position.x));
+	else if(p->position.x<1000){
+		 string_of_x = ("0" + to_string(p->position.x));
 	}
-	else if (p->position.x < 10000)
-	{
-		string_of_x = to_string(p->position.x);
+	else if(p->position.x<10000){
+		 string_of_x = to_string(p->position.x);
 	}
 
-	if (p->position.y < 10)
-	{
-		string_of_y = ("0" + ("0" + ("0" + to_string(p->position.y))));
+	if(p->position.y<10){
+		 string_of_y = ("0" + ("0" + ("0" + to_string(p->position.y))));
 	}
-	else if (p->position.y < 100)
-	{
-		string_of_y = ("0" + ("0" + to_string(p->position.y)));
+	else if(p->position.y<100){
+		 string_of_y = ("0" + ("0" + to_string(p->position.y)));
 	}
-	else if (p->position.y < 1000)
-	{
-		string_of_y = "0" + to_string(p->position.y);
+	else if(p->position.y<1000){
+		 string_of_y = "0" + to_string(p->position.y);
 	}
-	else if (p->position.y < 10000)
-	{
+	else if(p->position.y<10000){
 		string_of_y = to_string(p->position.y);
 	}
 
-	string sending_data = string_of_x + "_" + string_of_y + "_" + to_string(p->position.w) + "_" + to_string(p->position.h) + "_" + to_string(p->GameWon) + "_" + to_string(p->quit) + "_" + to_string(p->CompletedMilestones);
+	string sending_data = string_of_x + "_" + string_of_y + "_" + to_string(p->position.w) + "_" + to_string(p->position.h) + "_" + to_string(p->GameWon) +  "_" + to_string(p->quit) +  "_" + to_string(p->CompletedMilestones);
 	int n = sending_data.length();
 	strcpy(message, sending_data.c_str());
 	int size = send(new_socket, &message, strlen(message), 0);
 
-	if (size < 0)
-	{
+	if(size<0){
 		printf("error size < 0");
 	}
 	return message;
@@ -122,19 +111,18 @@ void server::recvData(Player *p)
 	extract_data(buffer, p->position.x, p->position.y, p->position.w, p->position.h, p->GameWon, p->quit, p->CompletedMilestones);
 }
 
-int main()
-{
+int main(){
 	server *head = nullptr;
 	head = new server();
 	Player *newplayer_1 = nullptr;
 	newplayer_1 = new Player();
-	newplayer_1->position.x = 0;
-	newplayer_1->position.y = 1;
-	newplayer_1->position.w = 2;
-	newplayer_1->position.h = 3;
-	newplayer_1->GameWon = 4;
-	newplayer_1->quit = 5;
-	newplayer_1->CompletedMilestones = 6;
+	newplayer_1->position.x=0;
+	newplayer_1->position.y=1;
+	newplayer_1->position.w=2;
+	newplayer_1->position.h=3;
+	newplayer_1->GameWon=4;
+	newplayer_1->quit=5;
+	newplayer_1->CompletedMilestones=6;
 	head->setupConnection();
 	string new_string = head->sendData(newplayer_1);
 	cout << new_string << endl;
